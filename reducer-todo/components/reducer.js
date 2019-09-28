902bc84c286a9ba993f079239b9335ac6fe0c7f9
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-09-27 17:00:16 -0700

    commit

---
+++ reducer-todo/components/reducer.js
@@ -0,0 +1,10 @@
+export const initialState = {
+     id: 1,
+     name: "Reducer Example",
+     used: false
+
+  };
+
+  export const myReducer = (state, action) => {   
+        return state;
+    };
